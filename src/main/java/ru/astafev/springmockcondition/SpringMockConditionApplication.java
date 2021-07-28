3e2d76f842e6522e9445957cf1f9206e9b946807
Evgeny <astafev.evgeny@gmail.com>
2021-07-28 10:37:51 +0500

    generated project

---
+++ src/main/java/ru/astafev/springmockcondition/SpringMockConditionApplication.java
@@ -0,0 +1,13 @@
+package ru.astafev.springmockcondition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMockConditionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMockConditionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/astafev/springmockcondition/SpringMockConditionApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.astafev.springmockcondition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMockConditionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
